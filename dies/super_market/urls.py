ea86f7b7011b38f117fe7e0ab9d2e4bc3f54c782
ronsalazar08 <ronsalazar01@gmail.com>
2020-12-04 13:27:03 +0800

    olo

--- dies/super_market/urls.py
+++ dies/super_market/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.data_input, name='super_data_input'),
     path('details/<slug:part_id>', views.details, name='part_id_details'),
+    path('claim/<slug:part_id>', views.claim, name='part_id_claim'),
 ]
--- dies/super_market/views.py
+++ dies/super_market/views.py
@@ -3,14 +3,27 @@ from django.shortcuts import render
 from .models import *
 
 def data_input(request):
-    
     return render(request, 'super_market/data_input.html')
 
-def details(request, part_id):
 
+def details(request, part_id):
     try:
         item = records.objects.get(part_id=part_id)
     except:
         item = None
+    
+    p_id = part_id
+        
+    context = {
+        'item' : item,
+        'p_id' : p_id,
+    }
 
-    return render(request, 'super_market/part_id_details.html', {'item' : item})
+    return render(request, 'super_market/part_id_details.html', context)
+
+
+def claim(request, part_id):
+    item = records.objects.get(part_id=part_id)
+    item.status_id = False
+    item.save()
+    return render(request, 'super_market/part_id_claim.html', {'item' : part_id})
